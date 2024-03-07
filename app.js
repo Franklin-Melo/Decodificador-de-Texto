17a92dde4051016b06482f800bd9096316af88f0
Franklin Melo <franklin.redes@gmail.com>
2024-03-07 15:47:17 -0300

    Decodificador de Texto

---
+++ app.js
@@ -0,0 +1,90 @@
+const placeholder = document.querySelector(".placeholder");
+const mensagem = document.querySelector(".mensagem");
+
+let matriz = [
+  ["e", "enter"],
+  ["i", "imea"],
+  ["a", "ai"],
+  ["o", "obet"],
+  ["u", "ufat"],
+];
+
+function btnCriptografar() {
+  const texto = placeholder.value;
+  const textoCriptografado = criptografar(texto);
+  // mensagem.innerHTML = `<h3>Mensagem Criptografada</h3><p>${textoCriptografado}</p>`;
+  mensagem.innerHTML = textoCriptografado;
+  placeholder.value = "";
+
+}
+
+function criptografar(string) {
+  const resultado = string
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imea")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "obet")
+    .replaceAll("u", "ufat");
+
+  return resultado;
+
+}
+
+function btnDescriptografar() {
+  const texto = placeholder.value;
+  const textoDescriptografado = descriptografar(texto);
+  // mensagem.innerHTML = `<h3>Mensagem Criptografada</h3><p>${textoCriptografado}</p>`;
+  mensagem.innerHTML = textoDescriptografado;
+  placeholder.value = "";
+}
+
+
+function descriptografar(string) {
+  const resultado = string
+    .replaceAll("enter", "e")
+    .replaceAll("imea", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("obet", "o")
+    .replaceAll("ufat", "u");
+
+  return resultado;
+
+}
+
+
+
+function copiarTexto() {
+  const textoCriptografado = mensagem.textContent;
+  navigator.clipboard.writeText(textoCriptografado);
+}
+
+// const btnCopiar = document.querySelector(".btn__copiar");
+// btnCopiar.addEventListener("click", copiarTexto);
+
+
+
+
+
+
+
+// let teste = "texto de teste"
+
+// console.log(criptografar(teste))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
